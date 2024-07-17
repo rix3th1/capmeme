a3f75e864967d15281c6e82ecf3747003ae122da
Richard A. Rich <rojasricor@gmail.com>
2024-07-17 18:33:27 -0500

    style: update header font size for mobile view

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,6 +2,6 @@
 	class="mx-auto mb-12 flex w-full max-w-4xl flex-col items-center border-b border-white/20 py-7 backdrop-blur-sm md:flex-row md:justify-between"
 >
 	<button on:click class="mb-4 flex items-center text-white/70 md:mb-0">
-		<div class="ml-2 text-2xl font-bold">Captain America memes powered by AI</div>
+		<div class="ml-2 text-2xl font-bold md:text-xl">Captain America memes powered by AI</div>
 	</button>
 </div>
